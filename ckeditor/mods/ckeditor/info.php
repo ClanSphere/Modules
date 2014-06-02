47abe446b95ee6eef6b443ba30cde95197bd89aa
Hans-Joachim Piepereit <hajo@csphere.eu>
2014-06-02 23:46:22 +0200

    updated fckeditor filebrowser to 2.6.11

--- ckeditor/mods/ckeditor/info.php
+++ ckeditor/mods/ckeditor/info.php
@@ -8,7 +8,7 @@ $mod_info['released']   = '2013-03-05';
 $mod_info['creator']    = 'CKSource';
 $mod_info['team']       = 'CKSource';
 $mod_info['url']        = 'www.ckeditor.com';
-$mod_info['text']       = 'Version 3.6.6.1 with FCKEditor Filebrowser 2.6.9';
+$mod_info['text']       = 'Version 3.6.6.1 with FCKEditor Filebrowser 2.6.11';
 $mod_info['icon']       = 'ckeditor';
 $mod_info['show']       = array('abcode/rte_html' => 5, 'abcode/rte_more' => 5, 'options/roots' => 5);
 $mod_info['categories'] = FALSE;
